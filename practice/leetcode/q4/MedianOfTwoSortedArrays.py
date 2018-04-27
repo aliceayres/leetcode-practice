f23c0ca3f78198d3f72e86776593e93038d4937f
alicearyes <alicearyes@qq.com>
2018-04-27 23:32:01 +0800

    question 4 not fix

--- practice/leetcode/q4/MedianOfTwoSortedArrays.py
+++ practice/leetcode/q4/MedianOfTwoSortedArrays.py
@@ -19,6 +19,87 @@ The median is (2 + 3)/2 = 2.5
 """
 
 class Solution:
+    def findMedianSortedArrays(self,nums1,nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m = len(nums1)
+        n = len(nums2)
+        odd = (m + n) % 2 == 1
+        number = []
+        if len(nums1) == 0 and len(nums2) == 0: # special situation
+            return 0.0
+        if len(nums1) == 0:
+            number = nums2
+        elif len(nums2) == 0:
+            number = nums1
+        elif nums1[-1] < nums2[0]:  # straightly extend
+            number.extend(nums1)
+            number.extend(nums2)
+        elif nums2[-1] < nums1[0]:
+            number.extend(nums2)
+            number.extend(nums1)
+        if len(number) != 0:
+            index = len(number) // 2
+            if odd:
+                return number[index] / 1.0
+            else:
+                return (number[index] + number[index - 1]) * 1.0 / 2
+# normal situation
+        k = (m + n + 1) // 2
+        search = nums1
+        other = nums2
+        end = m - 1
+        start = 0
+        if m > n:
+            search = nums2
+            other = nums1
+            t = n
+        while True:
+            p = (end + start) // 2
+            q = k - p - 2
+            if start > end:
+                print('&&&')
+                break;
+            if p > len(search)-1:
+                median = other[q]
+                median2 = other[q + 1]
+                break;
+            if q > len(other)-1:
+                median = search[p]
+                median2 = search[p + 1]
+                break;
+            if p+1 > len(search)-1:
+                median = max(search[p], other[q])
+                median2 = other[q + 1]
+                break;
+            if q+1 > len(other)-1:
+                median = max(search[p], other[q])
+                median2 = search[p+1]
+                break;
+            if p < 0:
+                median = other[q]
+                median2 = min(search[p + 1], other[q + 1])
+                break;
+            if q < 0:
+                median = search[p]
+                median2 = min(search[p + 1], other[q + 1])
+                break;
+            if search[p] <= other[q+1] and other[q] <= search[p+1]:
+                median = max(search[p], other[q])
+                median2 = min(search[p + 1], other[q + 1])
+                break;
+            if search[p] > other[q+1]: # left search
+                end = p -1
+            if other[q] > search[p+1]: # right search
+                start = p + 1
+        if odd:
+            return 2 * median / 2.0
+        else:
+            return (median + median2) / 2.0
+
     def findMedianSortedArraysNotLogMN(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -66,8 +147,10 @@ class Solution:
             return (number[index]+number[index+1])*1.0/2
 
 if __name__ == '__main__':
-    a = [5,6,7]
-    b = [1,2]
+    a = [4,5]
+    b = [1,2,3,6]
     s = Solution()
-    median = s.findMedianSortedArraysNotLogMN(b,a)
+    median = s.findMedianSortedArrays(b,a)
+    median2 = s.findMedianSortedArrays(b,a)
     print(median)
+    print(median2)
