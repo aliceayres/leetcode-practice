92bcff51fb99a1df73527882125e4f6bb44eb6da
alicearyes <alicearyes@qq.com>
2018-05-02 20:52:12 +0800

    question 283

--- practice/leetcode/q283/MoveZeroes.py
+++ practice/leetcode/q283/MoveZeroes.py
@@ -15,20 +15,46 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        zeros = 0
-        i = 0
-        begin =0
-        move = 0
+        total = 0
+        begin = 0
+        pos = 0
+        c = 0
         while True:
-            if begin + zeros == len(nums):
+            if pos + total == len(nums):
                 break
-            while nums[i] == 0:
+            zeros = 0
+            i = begin
+            while i < len(nums) and nums[i] == 0 :
                 zeros = zeros+1
                 i = i+1
+                c = c + 1
+                print(c)
+            move = i
+            j = pos
+            while move < len(nums) and nums[move] != 0:
+                nums[j] = nums[move]
+                j = j + 1
+                move = move +1
+                c = c + 1
+                print(c)
+            begin = move
+            pos = j
+            total = total + zeros
+        for k in range(pos,len(nums)):
+            nums[k] = 0
 
+    def moveZerosBySwap(self, nums):
+        for i in range(len(nums)):
+            if nums[i] ==0:
+                for j in range(i,len(nums)):
+                    if nums[j] != 0:
+                        nums[i] = nums[i]+nums[j]
+                        nums[j] = nums[i]-nums[j]
+                        nums[i] = nums[i]-nums[j]
+                        break
 
 if __name__ == '__main__':
-    array = [0,1,0,3,12]
+    array = [0,0,4,0,5,0,0,1,3,7,8,0,12]
     s = Solution()
     s.moveZeroes(array)
     print(array)
