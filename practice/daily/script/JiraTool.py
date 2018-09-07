f8cce3f21d62cb1a0a25ef28a1c0da9f7ee3ff8f
CTSIG <yeziqian@ctsig.com>
2018-09-07 18:58:04 +0800

    jira tool

--- practice/daily/script/JiraTool.py
+++ practice/daily/script/JiraTool.py
@@ -1,10 +1,13 @@
 from jira import JIRA
+import time
+from datetime import datetime
 import sys
 
 class JiraTool:
     def __init__(self):
         self.server = 'http://192.168.8.6:8080/'
-        self.basic_auth = ('username', 'password')
+        self.username = 'username'
+        self.basic_auth = (self.username, 'password')
         self.jiraClient = None
         self.login()
 
@@ -15,27 +18,62 @@ class JiraTool:
         else:
             return False
 
-    def client(self):
-        return self.jiraClient
+    def worklogs(self, issue):
+        return self.jiraClient.worklogs(issue)
+
+    def addWorklog(self,worklog):
+        return self.jiraClient.add_worklog(issue=worklog.issue, timeSpent=worklog.timeSpent, reduceBy=worklog.timeSpent,
+                                              comment=worklog.comment, user=self.username)
 
 class WorkLog:
     def __init__(self,issue,comment):
         self.issue = issue
         self.comment = comment
         self.timeSpent = '1d'
-        self.timeSpentSeconds = 28800
-        self.user = 'username'
-        self.tool = JiraTool()
 
-    def addWorkLog(self):
-        return self.tool.client().add_worklog(issue=self.issue, timeSpent=self.timeSpent,reduceBy=self.timeSpent,comment=self.comment, user=self.user)
+def worklog(tool,issue,comment):
+    display = ''
+    print('正在登记[%s]工时，内容为[%s]' % (issue, comment))
+    today_worklog = WorkLog(issue, comment)
+    result = tool.addWorklog(today_worklog)
+    print('登记工时[%s]成功！' % result)
+    display += '\n' + str(datetime.now())
+    display += '\n正在登记[{0}]工时，内容为[{1}]'.format(issue, comment)
+    display += '\n登记工时[{0}]成功！'.format(result)
+    return display
 
+def appendLogFile(display):
+    f = open('C:\\Users\\CTSIG\\work.log', 'a')
+    f.write(display)
+    f.close()
 
-# issue_id = sys.argv[1]
-# worklog_comment = sys.argv[2]
-issue_id = 'GEARS-752'
-worklog_comment = '登记工作'
-print('正在登记[%s]工时，内容为[%s]' % (issue_id,worklog_comment))
-today_worklog = WorkLog(issue_id,worklog_comment)
-result = today_worklog.addWorkLog()
-print('登记工时[%s]成功！' % result)
+def jiraWorklog(issue,comment):
+    display = '\n'
+    tool = JiraTool()
+    logs = tool.worklogs(issue)
+    lastlog = None
+    if logs is not None and len(logs) != 0:
+        lastlog = logs[-1]
+    if lastlog:
+        last_date = lastlog.created[0:10]
+        current = datetime.now()
+        if last_date == str(current)[0:10]:
+            repeated_display = '今日[{0}]已登记过工时[{1}]'.format(last_date, lastlog.id)
+            print(repeated_display)
+            display += repeated_display
+        else:
+            display += worklog(tool,issue,comment)
+    else:
+        display += worklog(tool,issue,comment)
+    appendLogFile(display)
+
+def task():
+    issue_id = 'GEARS-752'
+    worklog_comment = '根据IDCMS数据库设计文档，继续建表 '
+    from_sys_argvs = False
+    if from_sys_argvs:
+        issue_id = sys.argv[1]
+        worklog_comment = sys.argv[2]
+    jiraWorklog(issue_id,worklog_comment)
+
+task()
