d2769f9c657e06c72e88a0ebad6c64e38f837975
CTSIG <yeziqian@ctsig.com>
2019-10-14 17:57:12 +0800

    top k

--- practice/autumn/MInimum.py
+++ practice/autumn/MInimum.py
@@ -1,5 +1,6 @@
 '''
 Minimum 最小值
+同时求最大值和最小值
 '''
 
 
@@ -13,9 +14,35 @@ class Solution:
                 mini = e
         return mini
 
+    def double(self,num):
+        if len(num) == 0:
+            return None
+        min = num[0]
+        max = num[0]
+        i = 0
+        while i < len(num):
+            if i + 1 < len(num):
+                if num[i] <= num[i+1]:
+                    if num[i] < min:
+                        min = num[i]
+                    if num[i+1] > max:
+                        max = num[i+1]
+                else:
+                    if num[i+1] < min:
+                        min = num[i+1]
+                    if num[i] > max:
+                        max = num[i]
+            else:
+                if num[i] < min:
+                    min = num[i]
+                if num[i] > max:
+                    max = num[i]
+            i += 2
+        return min,max
 
 if __name__ == '__main__':
     slt = Solution()
-    num = [1,6,22,9,-13,0,2,1]
+    num = [1,6,22,9,-13,0,2,1,89]
     result = slt.solute(num)
-    print(result)     
+    print(result)
+    print(slt.double(num))
--- practice/autumn/krank.py
+++ practice/autumn/krank.py
@@ -1,12 +1,16 @@
 '''
 Kth smallest rank 第k小的值
+堆排序 归并排序 nlgn
+期望线性时间 所有元素互异 n 最坏 n^2 按余下元素中最大的划分
+最坏线性时间 n
+基于循环 9.2-3
 '''
 
 import random
 
 class Solution:
     def solute(self,num,k):
-        return self.exlinear(num,0,len(num)-1,k)
+        return self.exlinear_loop(num,0,len(num)-1,k)
 
     def exlinear(self,num,begin,end,k):
         print(num[begin:end + 1],k)
@@ -21,21 +25,38 @@ class Solution:
             return self.exlinear(num,pivot+1,end,k-pivot-1+begin)
 
 
+    def exlinear_loop(self,num,p,q,r):
+        begin = p
+        end = q
+        kk = r
+        while True:
+            print(num[begin:end + 1], kk)
+            if begin == end:
+                return num[begin]
+            pivot = self.randpart(num, begin, end)
+            if kk == pivot + 1 - begin:  # don't forget begin
+                return num[pivot]
+            elif kk < pivot + 1 - begin:
+                end = pivot - 1
+            else:
+                kk = kk - pivot - 1 + begin  # here begin will change , calculate new kk first!
+                begin = pivot + 1
+
+
     def randpart(self,num,begin,end):
+        print(begin,end)
         rand = random.randint(begin,end)
         self.exch(num,begin,rand)
         x = num[begin]
+        print(x)
         i = begin
         j = begin
-        print(x)
         while j <= end:
             if num[j] < x:
                 i += 1
                 self.exch(num,i,j)
             j += 1
-
         self.exch(num,i,begin)
-        print(num[begin:end+1])
         return i
 
     def exch(self,num,a,b):
@@ -47,6 +68,6 @@ class Solution:
 if __name__ == '__main__':
     slt = Solution()
     num = [1, 3, 9, 2, 6, 4, 5, 7, 8, 10]
-    k = 10
-    result = slt.solute(num,7)
+    k = 5
+    result = slt.solute(num,k)
     print(result)
