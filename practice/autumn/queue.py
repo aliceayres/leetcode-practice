2186396ef643719783c2d4d9a7ef9fb802244d1c
CTSIG <yeziqian@ctsig.com>
2019-10-21 11:51:15 +0800

    circle queue

--- practice/autumn/queue.py
+++ practice/autumn/queue.py
@@ -1,24 +1,67 @@
 '''
-Queue 队列
+Queue 循环队列 10.1.4
 '''
 
 
 class Queue:
-    def __init__(self):
-        self.queue = []
+    def __init__(self, size):
+        self.queue = [None for i in range(size)]
+        self.size = size
+        self.top = 0    # head
+        self.tail = 0   # tail to put
+
+    def isempty(self):
+        if self.top == self.tail:
+            return True
 
     def enqueue(self,x):
-        self.stack.append(x)
+        if self.top == self.tail and self.queue[self.tail] is not None:
+            raise Exception('queue upflow')
+        self.queue[self.tail] = x
+        self.tail = (self.tail + 1) % self.size
         return
 
     def dequeue(self):
-        return self.stack.pop(0)
+        if self.queue[self.top] is None:
+            raise Exception('queue underflow')
+        head = self.queue[self.top]
+        self.queue[self.top] = None
+        self.top = (self.top + self.size + 1) % self.size
+        return head
 
 
 if __name__ == '__main__':
-    q = Queue()
-    num = ["a","b","c","d","e","f","g","h"]
+    q = Queue(10)
+    # print(q.dequeue())
+    num = ["a","b","c","d","e","f","g","h","i","j"]
     for e in num:
         q.enqueue(e)
-    for i in range(len(num)+1):
+    # q.enqueue('m')
+    for i in range(8):
         print(q.dequeue())
+    print(q.queue, q.top, q.tail)
+    q.enqueue('m')
+    q.enqueue('n')
+    for i in range(3):
+        print(q.dequeue())
+    print(q.queue, q.top, q.tail)
+    # print(q.queue,q.top,q.tail)
+    # print(q.top)
+    # print(q.tail)
+    # q.enqueue('x')
+    # print(q.queue,q.top,q.tail)
+    # q.enqueue('y')
+
+    # q.enqueue('x')
+    # q.enqueue('y')
+    # print(q.queue,q.top,q.tail)
+    # print(q.dequeue())
+    # print(q.dequeue())
+    # q.enqueue('x')
+    # print(q.queue,q.top,q.tail)
+    # print(q.dequeue())
+
+
+
+
+
