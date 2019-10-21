54d2ade4489efab7e066b4880819536af88f0b6e
CTSIG <yeziqian@ctsig.com>
2019-10-21 18:17:59 +0800

    deque

---
+++ practice/autumn/doublelinkedlist.py
@@ -0,0 +1,22 @@
+'''
+Double linked list
+双链表
+'''
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoubleLinkList:
+    def list_search(self,head,k):
+        return
+
+    def list_head_insert(self,head,x):
+        return
+
+    def list_delete(self,head,x):
+        return
+
+if __name__ == '__main__':
+    dll = DoubleLinkList()
