e70b65b18af126b9d1d7e7570af939ee55f82e55
CTSIG <yeziqian@ctsig.com>
2019-11-05 18:53:40 +0800

    脚本

--- practice/daily/script/oss.py
+++ practice/daily/script/oss.py
@@ -17,10 +17,46 @@ class Record:
         self.raw.append(type)
         self.raw.append(name)
 
-def tablesFromExcel(dir,filename,name):
+class Analysis:
+    def __init__(self):
+        self.cache = {}
+        self.data = []
+
+    def analysis(self):
+        print(self.cache)
+        for item in self.cache.items():
+            dt = []
+            dt.append(str(item[0]))
+            dt.append(str(item[1][2]))
+            dt.append(str(26 - item[1][2]))
+            dt.append(str(item[1][3]))
+            dt.append(str(24 - item[1][3]))
+            dt.append(str('%.2f' % (item[1][2]/26 * 100)))
+            dt.append(str('%.2f'% (item[1][3]/24* 100)))
+            dt.append(str(item[1][2] + item[1][3]))
+            dt.append(str(50 - (item[1][2] + item[1][3])))
+            dt.append(str('%.2f'% ((item[1][2] + item[1][3]) / 50 * 100)))
+            self.data.append(','.join(dt))
+        self.tocsv()
+
+    def tocsv(self):
+        root = r'C:\Users\CTSIG\Desktop\评审总结呵呵呵'
+        if not os.path.exists(root):
+            os.makedirs(root)
+        total_filename = 'tablesana.csv'
+        file = open(root + '\\' + total_filename, 'w', encoding="utf-8")
+        title = ['系统', '概要通过', '概要不通过','详细通过','详细不通过','概要通过率','详细通过率', '总通过', '总不通过', '总通过率']
+        file.write(','.join(title) + '\n')
+        for record in self.data:
+            file.write(record+ '\n')
+        file.close()
+
+def tablesFromExcel(dir,filename,name,analysis=None):
     workbook = xlrd.open_workbook(dir+filename)
     all_sheets = workbook.sheet_names()
     type = {2:'概要设计',3:'详细设计'}
+    if analysis is not None:
+        analysis.cache[name] = {2:0,3:0}
     for k in range(2,4):
         sheet = workbook.sheet_by_name(all_sheets[k])  # workbook.sheet_by_index(1)
         rows = sheet.nrows
@@ -28,6 +64,9 @@ def tablesFromExcel(dir,filename,name):
             record = Record(sheet.row_values(i), type[k], name)
             global all
             all.append(record)
+            if analysis is not None:
+                if record.raw[4] == '通过':
+                    analysis.cache[name][k] += 1
     return all
 
 def tablesToCsvFile():
@@ -46,6 +85,7 @@ def tablesToCsvFile():
     file.close()
 
 if __name__ == '__main__':
+    ana = Analysis()
     dir_name = r'C:\Users\CTSIG\Desktop\评审总结呵呵呵\hhh5'
     list = []
     for files in os.walk(dir_name):
@@ -56,6 +96,7 @@ if __name__ == '__main__':
     global all
     all = []
     for i in range(len(list)):
-        tablesFromExcel(dir_name+'\\',list[i],names[i])
+        tablesFromExcel(dir_name+'\\',list[i],names[i],analysis=ana)
     tablesToCsvFile()
+    ana.analysis()
 
